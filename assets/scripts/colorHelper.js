7d33085cee4164b34a1685d3abfc9dad50ddcc18
Joe P Shoulak <joe@squarecompass.io>
2023-03-22 12:34:55 -0500

    post-bootcamp cleanup

---
+++ assets/scripts/colorHelper.js
@@ -0,0 +1,38 @@
+const namedColorFile = "/assets/data/namedColors.json";
+
+const parseHex = hex => {
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+
+  return [r, g, b];
+};
+
+const differenceBewteenColors = (color1, color2) => {
+  let [r1, b1, g1] = parseHex(color1);
+  let [r2, b2, g2] = parseHex(color2);
+
+  let dR = Math.abs(r1 - r2);
+  let dG = Math.abs(g1 - g2);
+  let dB = Math.abs(b1 - b2);
+
+  return dR + dG + dB;
+};
+
+const findClosestColor = async color => {
+  let closestName = "";
+  let closestDiff = 1000;
+
+  const namedColors = await fetch(namedColorFile).then(res => res.json());
+
+  Object.entries(namedColors).forEach(([name, hex]) => {
+    let currentDiff = differenceBewteenColors(hex, color);
+
+    if (currentDiff < closestDiff) {
+      closestDiff = currentDiff;
+      closestName = name;
+    }
+  });
+
+  return { name: closestName, hex: namedColors[closestName] };
+};
